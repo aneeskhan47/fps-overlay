029ec053118c2d7f2148b0a74c3fe9de4f2bd87c
Anees Khan <kingkhan2388@gmail.com>
2026-05-02 13:46:49 +0300

    Update version to v1.5.3-beta in main.cpp and resource.rc

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,7 @@
 #define IDM_RESET_POS 1006
 
 // Current version
-#define APP_VERSION "v1.5.2-beta"
+#define APP_VERSION "v1.5.3-beta"
 
 // Settings window: fixed outer width (px), vertical resize minimum outer height
 static const int kConfigDlgOuterW = 420;
